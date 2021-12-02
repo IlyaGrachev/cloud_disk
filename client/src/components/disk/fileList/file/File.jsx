e3e1d03b1f4ec1d41509cdaf20e4b81eedaaee4c
Ilia <workmailgray@gmail.com>
2021-12-02 17:24:38 +0300

    client: folder opening implementation

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import './file.css';
+import {useDispatch, useSelector} from 'react-redux'
 import DirLogo from '../../../../assets/img/dir.svg';
 import FileLogo from '../../../../assets/img/file.svg';
+import { pushToStack, setCurrentDir } from "../../../../redusers/fileReducer";
 
 const File = ({file}) => {
+	const dispatch = useDispatch()
+	const currentDir = useSelector(state => state.files.currentDir)
 
+	function openDirHandler(){
+		dispatch(pushToStack(currentDir))
+		dispatch(setCurrentDir(file._id))
+	}
 
 	return(
-		<div className='file'>
+		<div className='file' onClick={file.type === 'dir' ? () => openDirHandler() : ''}>
 			<img src={file.type === "dir" ? DirLogo : FileLogo} alt="" className="file__img" />
 			<div className="file__name">{file.name}</div>
 			<div className="file__date">{file.date.slice(0, 10)}</div>
