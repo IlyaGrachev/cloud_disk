643747f752f4d5ba55a12d03d9a01ad086e775f2
Ilia <workmailgray@gmail.com>
2021-12-03 10:53:57 +0300

    client: downloading files from client

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import './file.css';
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import DirLogo from '../../../../assets/img/dir.svg';
 import FileLogo from '../../../../assets/img/file.svg';
 import { pushToStack, setCurrentDir } from "../../../../redusers/fileReducer";
 
-const File = ({file}) => {
+const File = ({ file }) => {
 	const dispatch = useDispatch()
 	const currentDir = useSelector(state => state.files.currentDir)
 
-	function openDirHandler(){
-		dispatch(pushToStack(currentDir))
-		dispatch(setCurrentDir(file._id))
+	function openDirHandler(file) {
+		if (file.type === 'dir') {
+			dispatch(pushToStack(currentDir))
+			dispatch(setCurrentDir(file._id))
+		}
+
 	}
 
-	return(
-		<div className='file' onClick={file.type === 'dir' ? () => openDirHandler() : ''}>
+	return (
+		<div className='file' onClick={() => openDirHandler(file)}>
 			<img src={file.type === "dir" ? DirLogo : FileLogo} alt="" className="file__img" />
 			<div className="file__name">{file.name}</div>
 			<div className="file__date">{file.date.slice(0, 10)}</div>
