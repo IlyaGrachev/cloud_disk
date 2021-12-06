d92b0ff4e1218b68d709bcdb9acf79179e876948
Ilia <workmailgray@gmail.com>
2021-12-06 18:05:45 +0300

    client: download bar

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -5,6 +5,7 @@ import FileList from "./fileList/FileList";
 import './disk.css';
 import Popup from "./Popup";
 import { setCurrentDir, setPopupDisplay } from "../../redusers/fileReducer";
+import Uploader from "./uploader/Uploader";
 
 const Disk = () => {
 	const dispatch = useDispatch()
@@ -68,6 +69,7 @@ const Disk = () => {
 			</div>
 			<FileList />
 			<Popup />
+			<Uploader />
 
 		</div>
 		:
